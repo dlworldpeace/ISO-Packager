b486ade6cfd55c4b05184f0dd51da19462ac7b5a
dlworldpeace <xdl_line@hotmail.com>
2019-07-09 18:27:23 +0800

    Ensure jar packaging only starts if all .xsd are parsed successfully

--- parse_xsd.sh
+++ parse_xsd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+working_fine=true
+
 for filename in src/*.xsd; do
     `xjc -d src/main/java -p deskera.fintech.$(basename "$filename" .xsd) src/$(basename "$filename") -XautoNameResolution`
     dir="src/main/java/deskera/fintech/$(basename "$filename" .xsd)"
@@ -8,16 +10,23 @@ for filename in src/*.xsd; do
         echo "$(basename "$filename") is succesfully parsed"
     else
         echo "$(basename "$filename") fails to be parsed"
+        working_fine=false
     fi
 done
 
-`mvn package`
-jar="target/deskera-fintech-1.0.jar"
-if [ -f "$jar" ]    
+if [ "$working_fine" = true ] ;
 then
-    echo "jar package is successfully exported at target/"
+    echo "exporting as a jar package..."
+    `mvn package`
+    jar="target/deskera-fintech-1.0.jar"
+    if [ -f "$jar" ]
+    then
+        echo "jar package is successfully exported at target/"
+    else
+        echo "jar package fails to be exported"
+    fi
 else
-    echo "jar package fails to be exported"
+    echo "jar package is not exported because some .xsd file fails to be parsed"
 fi
 
 read -n 1 -s -r -p "Press any key to exit"
